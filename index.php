42aff4d905669ce486f710a76c82448c5c9d3c01
MANSHI-K <155805216+MANSHI-K@users.noreply.github.com>
2024-01-05 23:29:52 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,105 @@
+<html>
+
+<head>
+  <title></title>
+  <style type="text/css">
+    #main {
+      background-color: blue;
+      display: flex;
+      justify-content: space-between;
+      border-bottom-style: solid;
+      border-width: 1px;
+      border-color: blue;
+    }
+
+    #m1 img {
+      top: 10px;
+      height: 100px;
+      width: 100px;
+    }
+
+    #m2 ol li {
+      float: left;
+      padding-left: 20px;
+      list-style: none;
+      
+      font-size: 25px;
+    }
+    #m2 ol li a {
+      color: white;
+      text-decoration: none;
+    }
+    #m3 {
+      height: 30px;
+      width: 100%;
+      color: blue;
+    }
+    #m4 {
+
+      background-image: url('college.jpg');
+      height: 500px;
+      width: 100%;
+      background-size: cover;
+    }
+
+    #main4 {
+      background-color: rgb(12, 188, 64);
+      height: 60px;
+      width: 500px;
+      margin-left: 400px;
+      margin-top: 40px;
+      text-align: center;
+      font-size: 20px;
+    }
+
+    #main5 {
+      background-color: rgb(133, 134, 64);
+      height: 200px;
+      width: 500px;
+      margin-left: 400px;
+      margin-top: 25px;
+      text-align: center;
+    }
+
+  
+  </style>
+</head>
+
+<body>
+<div id="main">
+		<div id="m1"><img src="logo.jpg"></div>
+		<div id="m2">
+		  <ol>
+			 <li><a href="/kolhan.html">Home</a></li>
+			 <li><a href="aa.php">About Us</a></li>
+			 <li><a href="#">Contact Us</a></li>
+		  </ol>
+		</div>
+  </div>
+
+  <div id="m3">
+    <marquee direction="left">for any other information mail us at kuuniv@gmail.com and contact us 9471720475 time(10am
+      to 5pm)</marquee>
+  </div>
+
+
+  <div id="m4">
+  </div>
+
+  <div id="main2">
+    <div id="main4" style="background-color: red;">
+      <a href="kolhan2.php">Click Here to Apply for Migration</a>
+    </div>
+    <div id="main4"><a href="kolhan3.php">Click Here For Admit Card of FYUGP Semester-I Under NEP (2022-2026) Examination-2022</a>
+    </div>
+    <div id="main4"><a href="#">Provisional Marks of B.Ed Semester-II Examination 2022</a></div>
+    <div id="main5">
+      <h1>Provisional Marks of</h1>
+      <h2><a href="#">B.Ed Semester III Examiation 2023</a></h2>
+      <h3><a href="#">PG Semester I Examination 2022</a></h3>
+      <h4><a href="#">UG Semester II,IV & V Examination 2022</a></h4>
+    </div>
+    <div id="main4"><a href="#">Click Here for College Login of Internal Marks Entry (UG & PG CBCS System)</a></div>
+</body>
+
+</html>
